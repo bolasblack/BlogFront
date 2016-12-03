40c3689ab22b6e7c60d84c72ce537b61b3527016
c4605 <bolasblack@gmail.com>
2016-12-03 17:32:32 +0800

    增加了 utils.reArg

--- scripts/utils/index.js
+++ scripts/utils/index.js
@@ -7,20 +7,33 @@ import _endsWith from 'lodash/endsWith'
 export default {
   extend: _extend,
 
-  // String -> String
+  // camelCase :: String -> String
   camelCase: _camelCase,
 
-  // String -> String -> Boolean
+  // startsWith :: String -> String -> Boolean
   startsWith: R.curryN(2, R.flip(_startsWith)),
+
+  // endsWith :: String -> String -> Boolean
   endsWith: R.curryN(2, R.flip(_endsWith)),
 
-  // (a -> a) -> String -> {k: v} -> {k: v}
+  // adjustObj :: (a -> a) -> String -> {k: v} -> {k: v}
   adjustObj: R.curry((updater, propName, obj) => {
     return R.assoc(propName, updater(R.prop(propName, obj)), obj)
   }),
 
-  // [a] -> [a]
+  // compact :: [a] -> [a]
   compact: R.filter(R.identity),
+
+  // reArg :: [Number] -> (a -> b -> c -> d) -> (c -> b -> a -> d) | (*... -> d)
+  reArg: R.curry((argOrder, fn) => {
+    return R.curryN(argOrder.length, (...args) => {
+      const newArgs = argOrder.reduce((newArgs, targetPos, sourcePos) => {
+        newArgs[targetPos] = args[sourcePos]
+        return newArgs
+      }, [])
+      return fn(...newArgs)
+    })
+  }),
 
   // (a -> String) -> [a] -> [a]
   sortByDate: R.curry((getter, data) => {
